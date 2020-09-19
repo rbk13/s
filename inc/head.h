1c3a885dd8ecddf67900991b0db3db279316c512
rbk13 <67997475+rbk13@users.noreply.github.com>
2020-09-19 11:44:38 +0530

    Create head.h

---
+++ inc/head.h
@@ -0,0 +1,11 @@
+#include <limits.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+
+
+int isFull(struct Queue* queue)
+int isEmpty(struct Queue* queue)
+void enqueue(struct Queue* queue, int item) 
+int dequeue(struct Queue* queue) 
+int front(struct Queue* queue)
+int rear(struct Queue* queue)  
